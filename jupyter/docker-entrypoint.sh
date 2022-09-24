827967f201c7da20ffcaef8a3db20e7a7480d4ff
Stephen <37083603+st7ma784@users.noreply.github.com>
2022-09-24 01:30:58 +0100

    Update docker-entrypoint.sh

--- jupyter/docker-entrypoint.sh
+++ jupyter/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 sudo service munge start
+conda run -p /root/open-ce jupyter lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
 
-jupyter lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
-
-tail -f /dev/null
