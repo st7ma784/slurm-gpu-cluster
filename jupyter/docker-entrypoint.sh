06fff7552bed0baa6371357a22558f8494cbd820
Stephen <37083603+st7ma784@users.noreply.github.com>
2022-09-26 22:38:21 +0100

    Add files via upload

--- jupyter/docker-entrypoint.sh
+++ jupyter/docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sudo service munge start
-#conda run --name open-ce 
-sudo service slurmd start
+service munge start
 
-sudo jupyter lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
+jupyter lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
 
 tail -f /dev/null
